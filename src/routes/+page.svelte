66576547cabce38b8c0d0561c9ee90cfe8fcd262
Aero15 <Aero15@users.noreply.github.com>
2026-05-16 14:06:55 +0200

    [Shadcn] Install Shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
+<script lang="ts">
+	import { Button } from '$lib/shadcn/ui/button';
+</script>
+
 <svelte:head>
 	<title>Moja Besançon</title>
 </svelte:head>
 
 <h1>Accueil</h1>
+
+<Button variant="primary">Primary</Button>
+<Button>Secondary</Button>
